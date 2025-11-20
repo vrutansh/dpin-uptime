2881ab4b24dda6961291f575fb9b4008c61c514a
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-11-20 21:44:31 +0530

    routes added

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+import express from 'express';
+const app = express();
+
+app.post("/api/v1/website", (req, res) => {
+
+})
+
+
+app.get("/api/v1/website/status", (req, res) => {
+     
+})
+
+app.get('/api/v1/websites', (req, res) => {
+  
+})
+
+app.delete('/api/v1/website/', (req, res) => {
+  
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
