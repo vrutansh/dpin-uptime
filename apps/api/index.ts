b1e885ac0c28b80e62f44d5327d0cd4c7cc494c7
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-11-26 23:25:20 +0530

    middleware done

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,20 +1,22 @@
 import express from 'express';
+import { authMiddleware } from './middleware';
 const app = express();
 
-app.post("/api/v1/website", (req, res) => {
+app.post("/api/v1/website",authMiddleware, (req, res) => {
+  const userId = req.userId;
 
 })
 
 
-app.get("/api/v1/website/status", (req, res) => {
+app.get("/api/v1/website/status",authMiddleware, (req, res) => {
      
 })
 
-app.get('/api/v1/websites', (req, res) => {
+app.get('/api/v1/websites',authMiddleware, (req, res) => {
   
 })
 
-app.delete('/api/v1/website/', (req, res) => {
+app.delete('/api/v1/website/', authMiddleware, (req, res) => {
   
 })
 
