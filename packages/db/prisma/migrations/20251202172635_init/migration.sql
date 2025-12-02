3f548aac941294db50adb5a85ef0fe4686e78a75
Vrutansh Patel <vrutanshpatel01@gmail.com>
2025-12-02 22:58:05 +0530

    migrated db

---
+++ packages/db/prisma/migrations/20251202172635_init/migration.sql
@@ -0,0 +1,56 @@
+-- CreateEnum
+CREATE TYPE "WebsiteStatus" AS ENUM ('Good', 'Bad');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Website" (
+    "id" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Website_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Validator" (
+    "id" TEXT NOT NULL,
+    "publicKey" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "ip" TEXT NOT NULL,
+
+    CONSTRAINT "Validator_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WebsiteTick" (
+    "id" TEXT NOT NULL,
+    "websiteId" TEXT NOT NULL,
+    "validatorId" TEXT NOT NULL,
+    "status" "WebsiteStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "latency" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "WebsiteTick_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Website_url_key" ON "Website"("url");
+
+-- AddForeignKey
+ALTER TABLE "Website" ADD CONSTRAINT "Website_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WebsiteTick" ADD CONSTRAINT "WebsiteTick_websiteId_fkey" FOREIGN KEY ("websiteId") REFERENCES "Website"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WebsiteTick" ADD CONSTRAINT "WebsiteTick_validatorId_fkey" FOREIGN KEY ("validatorId") REFERENCES "Validator"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
